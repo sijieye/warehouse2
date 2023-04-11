41a5877ad3d5eb50c6815b7566cb65f559b65bbf
sijieye <sijieye@bu.edu>
2023-04-11 02:36:59 -0400

    Add frontend

--- frontend2/src/App.tsx
+++ frontend2/src/App.tsx
@@ -1,26 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 import logo from './logo.svg';
 import './App.css';
+import { SelectChangeEvent } from '@mui/material/Select';
+import Grid from "@mui/material/Unstable_Grid2";
+import { Button, Typography, FormGroup, TextField, Box, FormControl,  } from "@mui/material";
 
 function App() {
+
+  const [url, setURL] = useState<string>("");
+
+  // const handleSubmit = (event: SelectChangeEvent) => {
+  //   // Store value selected, so selection box will display it
+  //   setURL(event.target.value);
+
+  //   console.log(url)
+  // };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ width: "100vw", height: "100vh" }}>
+      <Grid container spacing={2} style={{ padding: "1rem" }}>
+        <Grid xs={12} container alignItems="center" justifyContent="center">
+          <Typography variant="h2" gutterBottom>
+            Insert Image URL
+          </Typography>
+        </Grid>
+      
+        <Grid xs={12} container alignItems="center" justifyContent="center">
+          <FormControl style={{display: "inline"}}>
+            <TextField InputProps={{style: {marginRight: "10px", width: '40em'}}} name="urlInsert" onInput={ event=>setURL((event.target as HTMLInputElement).value)} />
+            <Button style={{marginTop: "6px"}} type="submit" variant="outlined" onClick={() => alert(url)}>Submit</Button>
+          </FormControl>
+        </Grid>
+      </Grid>
     </div>
+
   );
+  
 }
 
 export default App;
--- warehousequeue/ocr/index.js
+++ warehousequeue/ocr/index.js
@@ -1,9 +1,31 @@
+const request = require('request');
+
 module.exports = async function (context, myQueueItem) {
+    
     context.log('JavaScript queue trigger function processed work item', myQueueItem);
-    context.log('expirationTime =', context.bindingData.expirationTime);
-    context.log('insertionTime =', context.bindingData.insertionTime);
-    context.log('nextVisibleTime =', context.bindingData.nextVisibleTime);
-    context.log('id =', context.bindingData.id);
-    context.log('popReceipt =', context.bindingData.popReceipt);
-    context.log('dequeueCount =', context.bindingData.dequeueCount);
+    context.log("Name:", process.env["ocrKey"])
+    
+    // var headersInfo = new Headers();
+    // headersInfo.append("Ocp-Apim-Subscription-Key", "26019dab75d94c86b52b07cebb546f23");
+    // headersInfo.append("Content-Type", "application/json");
+
+    const options = {
+        url: `https://${process.env["ocrURL"]}/computervision/imageanalysis:analyze?features=caption,read&model-version=latest&language=en&api-version=2023-02-01-preview`,
+        method: 'POST',
+        headers: {
+            'Ocp-Apim-Subscription-Key': process.env["ocrKey"],
+            'Content-Type': 'application/json'
+        },
+        body: "{'url':'https://learn.microsoft.com/azure/cognitive-services/computer-vision/media/quickstarts/presentation.png'}"
+    };
+    
+    function callback(error, response, body) {
+        if (!error && response.statusCode == 200) {
+            // console.log(body);
+            console.log("It works");
+
+        }
+    }
+    request(options, callback);
 };
+
