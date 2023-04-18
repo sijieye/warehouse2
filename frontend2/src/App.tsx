cbf74ed7ad4baf3ee34dc20412cfc60943167e98
sijieye <sijieye@bu.edu>
2023-04-18 02:22:11 -0400

    Debug deployment script 11

--- frontend2/src/App.tsx
+++ frontend2/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { SignedContext } from "./contexts/signedContext"
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
